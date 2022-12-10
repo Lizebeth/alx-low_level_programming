893184b800ebddb6b2ded6b3ed446b7db904ff2d
Lizebeth <Lizzy.taiye14@gmail.com>
2022-12-09 20:08:13 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all single digit number of base 10
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
