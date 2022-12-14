bb063eda0e54e10f22b0fedd3a3586b40fc886dd
Lizebeth <Lizzy.taiye14@gmail.com>
2022-12-13 21:10:56 -0800

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,7 +21,11 @@ void print_times_tables(int n);
 	_putchar(',');
 	_putchar(',');
 	prod = num * mult;
-	if (prod <= 100)
+	if (prod <= 99)
+	_putchar(' ');
+	if (prod <= 9)
+	_putchar(' ');
+	if (prod >= 100)
 	{
 	_putchar((prod / 100) + '0');
 	_putchar(((prod / 10)) % 10 + '0');
