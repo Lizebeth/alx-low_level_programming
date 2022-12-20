dfd088a6c9c70f7fcae850f035a275a7692b06a2
Lizebeth <Lizzy.taiye14@gmail.com>
2022-12-19 17:11:39 -0800

    task 4 arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_rev -> printing a string in reverse
@@ -12,11 +13,12 @@ void print_rev(char *s)
 
 	while (s[c] != '\0')
 	{
-	c++
+	c++;
 	}
 
 	for (c -= 1; c >= 0; c--)
 	{
 	_putchar(s[c]);
 	}
-	_putchar('\n')
+	_putchar('\n');
+}
